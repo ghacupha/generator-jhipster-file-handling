15d7d4bdc94ff0d9f1070361a9580956eaebcb54
ghacupha <mailnjeru@gmail.com>
2020-05-25 09:07:48 +0300

    Initialize project with jhipstermodule

---
+++ README.md
@@ -0,0 +1,56 @@
+# generator-jhipster-file-handling
+
+[![NPM version][npm-image]][npm-url] [![Build Status][github-actions-image]][github-actions-url] [![Dependency Status][daviddm-image]][daviddm-url]
+
+> JHipster module, File handling java api for uploading and reading data from a file
+
+# Introduction
+
+This is a [JHipster](https://www.jhipster.tech/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](https://www.jhipster.tech/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://www.jhipster.tech/installation/)
+
+# Installation
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-file-handling
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-file-handling
+```
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-file-handling
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-file-handling
+```
+
+# Usage
+
+# License
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-file-handling.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-file-handling
+[github-actions-image]: https://github.com/ghacupha/generator-jhipster-file-handling/workflows/Build/badge.svg
+[github-actions-url]: https://github.com/ghacupha/generator-jhipster-file-handling/actions
+[daviddm-image]: https://david-dm.org/ghacupha/generator-jhipster-file-handling.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/ghacupha/generator-jhipster-file-handling
---
+++ generators/app/index.js
@@ -0,0 +1,144 @@
+const chalk = require('chalk');
+const semver = require('semver');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+const packagejs = require('../../package.json');
+
+module.exports = class extends BaseGenerator {
+    get initializing() {
+        return {
+            init(args) {
+                if (args === 'default') {
+                    // do something when argument is 'default'
+                    this.message = 'default message';
+                }
+            },
+            readConfig() {
+                this.jhipsterAppConfig = this.getAllJhipsterConfig();
+                if (!this.jhipsterAppConfig) {
+                    this.error('Cannot read .yo-rc.json');
+                }
+            },
+            displayLogo() {
+                // it's here to show that you can use functions from generator-jhipster
+                // this function is in: generator-jhipster/generators/generator-base.js
+                this.printJHipsterLogo();
+
+                // Have Yeoman greet the user.
+                this.log(
+                    `\nWelcome to the ${chalk.bold.yellow('JHipster file-handling')} generator! ${chalk.yellow(`v${packagejs.version}\n`)}`
+                );
+            },
+            checkJhipster() {
+                const currentJhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
+                const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
+                if (!semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
+                    this.warning(
+                        `\nYour generated project used an old JHipster version (${currentJhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`
+                    );
+                }
+            }
+        };
+    }
+
+    prompting() {
+        const prompts = [
+            {
+                when: () => typeof this.message === 'undefined',
+                type: 'input',
+                name: 'message',
+                message: 'Please put something',
+                default: 'hello world!'
+            }
+        ];
+
+        const done = this.async();
+        this.prompt(prompts).then(answers => {
+            this.promptAnswers = answers;
+            // To access props answers use this.promptAnswers.someOption;
+            done();
+        });
+    }
+
+    writing() {
+        // read config from .yo-rc.json
+        this.baseName = this.jhipsterAppConfig.baseName;
+        this.packageName = this.jhipsterAppConfig.packageName;
+        this.packageFolder = this.jhipsterAppConfig.packageFolder;
+        this.clientFramework = this.jhipsterAppConfig.clientFramework;
+        this.clientPackageManager = this.jhipsterAppConfig.clientPackageManager;
+        this.buildTool = this.jhipsterAppConfig.buildTool;
+
+        // use function in generator-base.js from generator-jhipster
+        this.angularAppName = this.getAngularAppName();
+
+        // use constants from generator-constants.js
+        const javaDir = `${jhipsterConstants.SERVER_MAIN_SRC_DIR + this.packageFolder}/`;
+        const resourceDir = jhipsterConstants.SERVER_MAIN_RES_DIR;
+        const webappDir = jhipsterConstants.CLIENT_MAIN_SRC_DIR;
+
+        // variable from questions
+        if (typeof this.message === 'undefined') {
+            this.message = this.promptAnswers.message;
+        }
+
+        // show all variables
+        this.log('\n--- some config read from config ---');
+        this.log(`baseName=${this.baseName}`);
+        this.log(`packageName=${this.packageName}`);
+        this.log(`clientFramework=${this.clientFramework}`);
+        this.log(`clientPackageManager=${this.clientPackageManager}`);
+        this.log(`buildTool=${this.buildTool}`);
+
+        this.log('\n--- some function ---');
+        this.log(`angularAppName=${this.angularAppName}`);
+
+        this.log('\n--- some const ---');
+        this.log(`javaDir=${javaDir}`);
+        this.log(`resourceDir=${resourceDir}`);
+        this.log(`webappDir=${webappDir}`);
+
+        this.log('\n--- variables from questions ---');
+        this.log(`message=${this.message}`);
+        this.log('------\n');
+
+        if (this.clientFramework === 'react') {
+            this.template('dummy.txt', 'dummy-react.txt');
+        }
+        if (this.clientFramework === 'angularX') {
+            this.template('dummy.txt', 'dummy-angularX.txt');
+        }
+        if (this.buildTool === 'maven') {
+            this.template('dummy.txt', 'dummy-maven.txt');
+        }
+        if (this.buildTool === 'gradle') {
+            this.template('dummy.txt', 'dummy-gradle.txt');
+        }
+    }
+
+    install() {
+        const logMsg = `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
+
+        const injectDependenciesAndConstants = err => {
+            if (err) {
+                this.warning('Install of dependencies failed!');
+                this.log(logMsg);
+            }
+        };
+        const installConfig = {
+            bower: false,
+            npm: this.clientPackageManager !== 'yarn',
+            yarn: this.clientPackageManager === 'yarn',
+            callback: injectDependenciesAndConstants
+        };
+        if (this.options['skip-install']) {
+            this.log(logMsg);
+        } else {
+            this.installDependencies(installConfig);
+        }
+    }
+
+    end() {
+        this.log('End of file-handling generator');
+    }
+};
