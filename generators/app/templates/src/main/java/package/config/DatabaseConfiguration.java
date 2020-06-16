451836601c6ab6e3ac361b60a9dd3f6393ecfa29
Edwin Njeru <mailnjeru@gmail.com>
2020-06-16 15:17:58 +0300

    Configure scan annotations

--- generators/app/templates/src/main/java/package/config/DatabaseConfiguration.java
+++ generators/app/templates/src/main/java/package/config/DatabaseConfiguration.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"<%= packageName %>.repository","<%= packageName %>.internal"})
+@EnableElasticsearchRepositories(basePackages = {"<%= packageName %>.repository.search", "<%= packageName %>.internal"})
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
