b136771021e6d65b6355c24d79c02c171cd8bda6
Edwin Njeru <mailnjeru@gmail.com>
2020-07-07 12:29:41 +0300

    Minor fixes

--- generators/app/templates/src/test/java/package/internal/model/sampleDataModel/CurrencyTableEVMMappingTest.java
+++ generators/app/templates/src/test/java/package/internal/model/sampleDataModel/CurrencyTableEVMMappingTest.java
@@ -21,17 +21,8 @@ class CurrencyTableEVMMappingTest {
     @Test
     void conversionToDTO() {
 
-            CurrencyTableDTO dto1 = new CurrencyTableDTO();
-            dto1.setCountry("KENYA");
-            dto1.setCurrencyCode("KES");
-            dto1.setCurrencyName("KENYA SHILLING");
-            dto1.setLocality(LOCAL);
-
-            CurrencyTableDTO dto2 = new CurrencyTableDTO();
-            dto2.setCountry("UGANDA");
-            dto2.setCurrencyCode("UGX");
-            dto2.setCurrencyName("UGANDA SHILLING");
-            dto2.setLocality(FOREIGN);
+        CurrencyTableEVM evm1 = CurrencyTableEVM.builder().country("KENYA").currencyCode("KES").currencyName("SHILLING").locality("LOCAL").build();
+        CurrencyTableEVM evm2 = CurrencyTableEVM.builder().country("UGANDA").currencyCode("UGX").currencyName("SHILLING").locality("FOREIGN").build();
 
         assertThat(currencyTableEVMMapping.toValue2(evm1).getLocality()).isEqualTo(LOCAL);
         assertThat(currencyTableEVMMapping.toValue2(evm2).getLocality()).isEqualTo(FOREIGN);
@@ -39,6 +30,18 @@ class CurrencyTableEVMMappingTest {
 
     @Test
     void conversionToEVM() {
+
+        CurrencyTableDTO dto1 = new CurrencyTableDTO();
+        dto1.setCountry("KENYA");
+        dto1.setCurrencyCode("KES");
+        dto1.setCurrencyName("KENYA SHILLING");
+        dto1.setLocality(LOCAL);
+
+        CurrencyTableDTO dto2 = new CurrencyTableDTO();
+        dto2.setCountry("UGANDA");
+        dto2.setCurrencyCode("UGX");
+        dto2.setCurrencyName("UGANDA SHILLING");
+        dto2.setLocality(FOREIGN);
 
         CurrencyTableDTO dto1 = CurrencyTableDTO.builder().country("KENYA").currencyCode("KES").currencyName("KENYA SHILLING").locality(LOCAL).build();
         CurrencyTableDTO dto2 = CurrencyTableDTO.builder().country("UGANDA").currencyCode("UGX").currencyName("UGANDA SHILLING").locality(FOREIGN).build();
