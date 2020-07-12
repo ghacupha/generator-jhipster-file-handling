97115eb2edeaff9ed502e040a1328428f941cfbc
ghacupha <mailnjeru@gmail.com>
2020-07-12 17:11:43 +0300

    minor cleanup

--- generators/app/templates/src/main/java/package/internal/service/HandlingService.java
+++ generators/app/templates/src/main/java/package/internal/service/HandlingService.java
@@ -1,10 +1,5 @@
 package <%= packageName %>.internal.service;
 
-import <%= packageName %>.internal.model.FileNotification;
-import <%= packageName %>.service.dto.CurrencyMainMessageTokenDTO;
-
-import java.util.concurrent.CompletableFuture;
-
 /**
  * This service the uploaded file in whatever way that is deemed necessary.
  * This service returns an instance of itself inorder to run withing a chain of file-handlers
